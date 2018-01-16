8237f2536982df19f98a99e3dab3cd11af130efb
Dmitrii Evsiukov <d.evsyukov@gmail.com>
2018-01-16 16:14:55 +0200

    Add empty template for new indicator Tunnel

---
+++ project/OsEngine/Charts/CandleChart/Indicators/Tunnel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using OsEngine.Entity;
+
+namespace OsEngine.Charts.CandleChart.Indicators
+{
+    public class Tunnel : IIndicatorCandle
+    {
+        public Tunnel(bool canDelete, string name)
+        {
+            CanDelete = canDelete;
+            Name = name;
+        }
+
+        public Tunnel(bool canDelete)
+        {
+            CanDelete = canDelete;
+        }
+
+        public IndicatorOneCandleChartType TypeIndicator { get; set; }
+        public List<Color> Colors { get; }
+        public List<List<decimal>> ValuesToChart { get; }
+        public bool CanDelete { get; set; }
+        public string NameSeries { get; set; }
+        public string NameArea { get; set; }
+        public string Name { get; set; }
+        public bool PaintOn { get; set; }
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowDialog()
+        {
+            throw new NotImplementedException();
+        }
+
+        public event Action<IIndicatorCandle> NeadToReloadEvent;
+        public void Process(List<Candle> candles)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
