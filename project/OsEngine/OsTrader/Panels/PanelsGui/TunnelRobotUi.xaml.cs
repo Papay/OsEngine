5d7094b5bb74133222427702e6e3774298d91646
Dmitrii Evsiukov <d.evsyukov@gmail.com>
2018-01-22 00:55:47 +0200

    Add settings panel for TunnelRobot

---
+++ project/OsEngine/OsTrader/Panels/PanelsGui/TunnelRobotUi.xaml.cs
@@ -0,0 +1,31 @@
+﻿/*
+ *Ваши права на использование кода регулируются данной лицензией http://o-s-a.net/doc/license_simple_engine.pdf
+*/
+
+using System;
+using System.Windows;
+using static OsEngine.OsTrader.Panels.PanelCreator;
+
+namespace OsEngine.OsTrader.Panels.PanelsGui
+{
+    /// <summary>
+    /// Логика взаимодействия для RobotUi.xaml
+    /// </summary>
+    public partial class TunnelRobotUi
+    {
+        private TunnelRobot robot;
+        public TunnelRobotUi(TunnelRobot robot)
+        {
+            InitializeComponent();
+            this.robot = robot;
+
+            this.ProfitBox.Text = this.robot.Profit.ValueDecimal.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.robot.Profit.ValueDecimal = Convert.ToDecimal(this.ProfitBox.Text);
+            Close();
+        }
+    }
+}
