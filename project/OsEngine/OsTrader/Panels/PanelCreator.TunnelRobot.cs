4d2e9d1b1df212b0f73f91d68f4efb5a84d7bcc4
Dmitrii Evsiukov <d.evsyukov@gmail.com>
2018-01-23 18:10:08 +0200

    Fixed changes in TunnelRobot

--- project/OsEngine/OsTrader/Panels/PanelCreator.TunnelRobot.cs
+++ project/OsEngine/OsTrader/Panels/PanelCreator.TunnelRobot.cs
@@ -8,6 +8,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading;
+using System.Windows.Automation.Peers;
 using System.Windows.Controls;
 using System.Windows.Forms.Integration;
 using System.Windows.Shapes;
@@ -39,7 +40,12 @@ namespace OsEngine.OsTrader.Panels
 
             private Tunnel tunnel;
 
-            private int Volume;
+            private int Volume1;
+            private int Volume2;
+            private int Volume3;
+            private int Volume4;
+            private bool[] OpenVolume;
+
             private BotTradeRegime regime;
 
             public StrategyParameterDecimal Profit;
@@ -50,10 +56,10 @@ namespace OsEngine.OsTrader.Panels
             public TunnelRobot(string name)
                 : base(name)
             {
-                this.Profit = CreateParameter("Profit", 0.0020m, 0.002m, 0.010m, 0.001m);
+                this.Profit = CreateParameter("Profit", 0.0012m, 0.001m, 0.10m, 0.0002m);
                 this.Slippage = CreateParameter("Slippage", 1, 0, 10, 1);
-                this.TunnelLength = CreateParameter("Tunnel.Length", 80, 20, 200, 5);
-                this.TunnelWidth = CreateParameter("Tunnel.Width", 80, 10, 100, 10);
+                this.TunnelLength = CreateParameter("Tunnel.Length", 90, 20, 200, 5);
+                this.TunnelWidth = CreateParameter("Tunnel.Width", 60, 10, 100, 10);
 
                 TabCreate(BotTabType.Simple);
                 this.bot = this.TabsSimple[0];
@@ -66,8 +72,14 @@ namespace OsEngine.OsTrader.Panels
                 this.tunnel = (Tunnel)this.bot.CreateCandleIndicator(this.tunnel, "Prime");
                 this.tunnel.Save();
 
-                this.Volume = 1;
+                this.Volume1 = 1;
+                this.Volume2 = 1;
+                this.Volume3 = 1;
+                this.Volume4 = 1;
 
+                this.OpenVolume = new bool[] {false, false, false, false };
+                
+                
                 this.regime = BotTradeRegime.Off;
 
                 this.bot.CandleFinishedEvent += this.OnCandleFinishedEvent;
@@ -100,7 +112,7 @@ namespace OsEngine.OsTrader.Panels
                 {
                     using (StreamReader reader = new StreamReader(@"Engine\" + NameStrategyUniq + @"SettingsBot.txt"))
                     {
-                        this.Volume = Convert.ToInt32(reader.ReadLine());
+                        this.Volume1 = Convert.ToInt32(reader.ReadLine());
                         Enum.TryParse(reader.ReadLine(), true, out this.regime);
 
                         reader.Close();
@@ -172,8 +184,19 @@ namespace OsEngine.OsTrader.Panels
                 decimal tunnelDown = this.tunnel.ValuesDown[this.tunnel.ValuesDown.Count - 1];
                 decimal slippage = this.Slippage.ValueInt * this.bot.Securiti.PriceStep;
 
-                this.bot.BuyAtStop(this.Volume, tunnelUp + slippage, tunnelUp, StopActivateType.HigherOrEqual);
-                this.bot.SellAtStop(this.Volume, tunnelDown, tunnelDown - slippage, StopActivateType.LowerOrEqyal);
+                if (lastCandle.High >= tunnelUp)
+                {
+                    //this.bot.BuyAtStop(this.Volume, tunnelUp + slippage, tunnelUp, StopActivateType.HigherOrEqual);
+                    this.bot.BuyAtLimit(this.Volume1 + this.Volume2 + this.Volume3, tunnelUp + slippage);
+                    this.OpenVolume = new bool[] { true, true, true, true };
+                }
+                    
+                if (lastCandle.Low <= tunnelDown)
+                {
+                    //this.bot.SellAtStop(this.Volume, tunnelDown, tunnelDown - slippage, StopActivateType.LowerOrEqyal);
+                    this.bot.SellAtLimit(this.Volume1 + this.Volume2 + this.Volume3, tunnelDown - slippage);
+                    this.OpenVolume = new bool[] { true, true, true, true };
+                }
             }
 
             /// <summary>
@@ -192,23 +215,99 @@ namespace OsEngine.OsTrader.Panels
                 switch (openPosition.Direction)
                 {
                     case Side.Buy:
-                        if (lastCandle.High >= tunnelUp + profit)
-                        {
-                            this.bot.CloseAtLimit(openPosition, tunnelUp + profit - slippage, openPosition.OpenVolume);
-                        }
-                        if (lastCandle.Close <= tunnelDown)
+                        if (lastCandle.Close <= tunnelDown && openPosition.OpenVolume > 0)
                         {
                             this.bot.CloseAtMarket(openPosition, openPosition.OpenVolume);
+                            //this.bot.SellAtLimit(this.Volume, tunnelDown - slippage);
+
+                            LogicOpenPosition(candles, null);
+                        }
+                        else
+                        {
+                            if (OpenVolume[3])
+                            {
+                                if (lastCandle.High >= tunnelUp + profit * 4)
+                                {
+                                    this.bot.CloseAtLimit(openPosition, tunnelUp + profit * 4 - slippage, this.Volume4);
+                                    OpenVolume[3] = false;
+                                }
+                            }
+                            if (OpenVolume[2])
+                            {
+                                if (lastCandle.High >= tunnelUp + profit * 3)
+                                {
+                                    this.bot.CloseAtLimit(openPosition, tunnelUp + profit * 3 - slippage, this.Volume3);
+                                    OpenVolume[2] = false;
+                                }
+                            }
+                            if (OpenVolume[1])
+                            {
+                                if (lastCandle.High >= tunnelUp + profit * 2)
+                                {
+                                    this.bot.CloseAtLimit(openPosition, tunnelUp + profit * 2 - slippage, this.Volume2);
+                                    OpenVolume[1] = false;
+                                }
+                            }
+                            if (OpenVolume[0])
+                            {
+                                if (lastCandle.High >= tunnelUp + profit * 1)
+                                {
+                                    this.bot.CloseAtLimit(openPosition, tunnelUp + profit * 1 - slippage, this.Volume1);
+                                    OpenVolume[0] = false;
+                                }
+                            }
+                            else
+                            {
+                                this.bot.CloseAtTrailingStop(openPosition, openPosition.EntryPrice, openPosition.EntryPrice);
+                            }
                         }
                         break;
                     case Side.Sell:
-                        if (lastCandle.Low <= tunnelDown - profit)
-                        {
-                            this.bot.CloseAtLimit(openPosition, tunnelDown - profit + slippage, openPosition.OpenVolume);
-                        }
-                        if (lastCandle.Close >= tunnelUp)
+                        if (lastCandle.Close >= tunnelUp && openPosition.OpenVolume > 0)
                         {
                             this.bot.CloseAtMarket(openPosition, openPosition.OpenVolume);
+                            //this.bot.BuyAtLimit(this.Volume, tunnelUp * k + slippage);
+
+                            LogicOpenPosition(candles, null);
+                        }
+                        else
+                        {
+                            if (OpenVolume[3])
+                            {
+                                if (lastCandle.Low <= tunnelDown - profit * 4)
+                                {
+                                    this.bot.CloseAtLimit(openPosition, tunnelDown - profit * 4 + slippage, this.Volume4);
+                                    OpenVolume[3] = false;
+                                }
+                            }
+                            if (OpenVolume[2])
+                            {
+                                if (lastCandle.Low <= tunnelDown - profit * 3)
+                                {
+                                    this.bot.CloseAtLimit(openPosition, tunnelDown - profit * 3 + slippage, this.Volume3);
+                                    OpenVolume[2] = false;
+                                }
+                            }
+                            if (OpenVolume[1])
+                            {
+                                if (lastCandle.Low <= tunnelDown - profit * 2)
+                                {
+                                    this.bot.CloseAtLimit(openPosition, tunnelDown - profit * 2 + slippage, this.Volume2);
+                                    OpenVolume[1] = false;
+                                }
+                            }
+                            if (OpenVolume[0])
+                            {
+                                if (lastCandle.Low <= tunnelDown - profit * 1)
+                                {
+                                    this.bot.CloseAtLimit(openPosition, tunnelDown - profit * 1 + slippage, this.Volume1);
+                                    OpenVolume[0] = false;
+                                }
+                            }
+                            else
+                            {
+                                this.bot.CloseAtTrailingStop(openPosition, openPosition.EntryPrice, openPosition.EntryPrice);
+                            }
                         }
                         break;
                 }
