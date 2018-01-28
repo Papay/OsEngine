1629d48cd211eb069a75415c92d4529c59b47e23
Dmitrii Evsiukov <d.evsyukov@gmail.com>
2018-01-29 01:37:04 +0200

    Change color of buy/sell signals on chart.

--- project/OsEngine/Charts/CandleChart/ChartPainter.cs
+++ project/OsEngine/Charts/CandleChart/ChartPainter.cs
@@ -1565,11 +1565,11 @@ namespace OsEngine.Charts.CandleChart
 
                                 if (openOrder.Side == Side.Buy)
                                 {
-                                    buySellSeries.Points[buySellSeries.Points.Count - 1].Color = Color.LimeGreen; // Color.ForestGreen;
+                                    buySellSeries.Points[buySellSeries.Points.Count - 1].Color = Color.Green;
                                 }
                                 else
                                 {
-                                    buySellSeries.Points[buySellSeries.Points.Count - 1].Color = Color.OrangeRed; // Color.Red;
+                                    buySellSeries.Points[buySellSeries.Points.Count - 1].Color = Color.Red;
 
                                 }
 
@@ -1604,11 +1604,11 @@ namespace OsEngine.Charts.CandleChart
 
                                 if (closeOrder.Side == Side.Buy)
                                 {
-                                    buySellSeries.Points[buySellSeries.Points.Count - 1].Color = Color.ForestGreen;
+                                    buySellSeries.Points[buySellSeries.Points.Count - 1].Color = Color.Yellow; // Color.ForestGreen;
                                 }
                                 else
                                 {
-                                    buySellSeries.Points[buySellSeries.Points.Count - 1].Color = Color.Red;
+                                    buySellSeries.Points[buySellSeries.Points.Count - 1].Color = Color.Yellow; // Color.Red;
                                 }
                                 if (_colorKeeper.PointType == PointType.TriAngle)
                                 {
